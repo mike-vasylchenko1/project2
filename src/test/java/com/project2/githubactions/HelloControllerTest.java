3cdb0cd6c573c06ce9ebf63cdb83c910fe98a117
volodymyrkotyk_ctr <volodymyrkotyk_ctr@fortna.com>
2021-06-28 14:09:15 +0300

    test project

---
+++ src/test/java/com/project2/githubactions/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.project2.githubactions;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	void hello() {
+		webTestClient.get()
+		             .uri("/test")
+		             .exchange()
+		             .expectStatus()
+		             .isOk()
+		             .expectBody(String.class)
+		             .isEqualTo("Hello World!");
+	}
+}
